14713be998f903678cf8f224a6953c092edb3aa2
jaren11 <jaren11@gmail.com>
2020-04-14 23:09:11 -0700

    please work

--- js/charityAPI.js
+++ js/charityAPI.js
@@ -4,29 +4,30 @@ document.getElementById("search").addEventListener("click", function(event) {
 
   name = name.replace(" ", "%20");
 
-  var state = document.getElementById('state').value;
+  var sort = document.getElementById('state').value
+
+  var state = document.getElementById('sort').value;
 
   if (name === "")
     return;
   console.log(name);
-  let url = "https://api.data.charitynavigator.org/v2/Organizations?app_id=7d03619f&app_key=f1fb0d396d1432517352d80d320a2e60&pageSize=20&search=" + name + "&state=" + state;
+  let url = "https://api.data.charitynavigator.org/v2/Organizations?app_id=7d03619f&app_key=f1fb0d396d1432517352d80d320a2e60&pageSize=20&search=" + name + "&state=" + state + "&sort=" + ;
   fetch(url)
     .then(function(response) {
       return response.json();
-
     }).then(function(json) {
       let results = "";
       for (let i=0; i < json.length; i++){
-        results += '<h2>Name of charity: ' + json[i].charityName + '</h2>';
+        results += '<div class="results"><h2>Name of charity: ' + json[i].charityName + '</h2>';
         if(json[i].tagLine === null){
-          results += '<h3>No Charity Tagline :(</h3>'
+          results += '<p>No Charity Tagline :(</p>'
         }else{
-          results += '<h3>Charity tagline:' + json[i].tagLine + "</h3>"
+          results += '<h3>Charity tagline: ' + json[i].tagLine + '</h3>'
         }
         if(json[i].mission === null){
-          results += '<h3>No Charity Mission :(</h3><br>'
+          results += '<p>No Charity Mission :(</p><br>'
         }else{
-          results += '<h3>Charity Mission:' + json[i].mission + "</h3><br>"
+          results += '<p>Charity Mission: ' + json[i].mission + "</p><br></div>"
         }
 
       }
